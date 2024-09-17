c7f70d0b8545e9a309b43b87952596e682c690e1
Bengt Hellstrom <bengt.hellstrom@umu.se>
2024-09-17 09:48:08 +0200

    Initial commit of spring-boot project

---
+++ src/main/java/eu/dc4eu/gateway/Dc4euEmrexGatewayApplication.java
@@ -0,0 +1,13 @@
+package eu.dc4eu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dc4euEmrexGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dc4euEmrexGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/dc4eu/gateway/Dc4euEmrexGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dc4eu.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dc4euEmrexGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
