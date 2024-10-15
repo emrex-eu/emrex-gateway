b3c59f7509aa742d4b1a1f64e668b81e2f5c2b7e
Johan Ekblad <johan.ekblad@umu.se>
2024-10-15 16:35:03 +0200

    Connecting the elmo->elm converter

---
+++ src/main/java/eu/dc4eu/gateway/ConverterService.java
@@ -0,0 +1,55 @@
+package eu.dc4eu.gateway;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import eu.dc4eu.gateway.converter.Parameters;
+import eu.dc4eu.gateway.converter.Protocol;
+import eu.dc4eu.gateway.converter.Request;
+import eu.dc4eu.gateway.converter.Response;
+import lombok.Getter;
+
+
+@Component
+@Getter
+public class ConverterService {
+
+	@Value("${dc4eu.converter.url}")
+	private String converterURL;
+
+	public Response convertElmoToElm(String elmo) {
+		Protocol fromProtocol = new Protocol();
+		fromProtocol.setName("elmo");
+		fromProtocol.setVersion("1.7");
+		Protocol toProtocol = new Protocol();
+		toProtocol.setName("elm");
+		toProtocol.setVersion("3.2");
+		Parameters parameters = new Parameters();
+		parameters.setPreferredLanguages(List.of("en", "sv", "no"));
+		Request request = new Request();
+		request.setFrom(fromProtocol);
+		request.setTo(toProtocol);
+		request.setParameters(parameters);
+		request.setContent(elmo);
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+
+		HttpEntity<Request> entity = new HttpEntity<>(request, headers);
+
+		ResponseEntity<Response> responseEntity = restTemplate.exchange(
+				converterURL, HttpMethod.POST, entity, Response.class);
+
+		return responseEntity.getBody();
+	}
+}
+
+
--- src/main/java/eu/dc4eu/gateway/controllers/EmcController.java
+++ src/main/java/eu/dc4eu/gateway/controllers/EmcController.java
@@ -1,6 +1,9 @@
 package eu.dc4eu.gateway.controllers;
 
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -13,16 +16,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import eu.dc4eu.gateway.ConverterService;
 import eu.dc4eu.gateway.EwpResponse;
+import eu.dc4eu.gateway.converter.Response;
 import eu.dc4eu.gateway.elmo.ElmoTojava;
 import eu.dc4eu.gateway.elmo.api.Elmo;
 import eu.dc4eu.gateway.emreg.EmregRepresentation;
 import eu.dc4eu.gateway.service.EmregCache;
 import eu.dc4eu.gateway.service.GatewaySession;
+import jakarta.inject.Inject;
 
 @Controller
 @RequestMapping("/emc")
 public class EmcController {
+
+	@Inject
+	private ConverterService converterService;
 
 	Logger logger = LoggerFactory.getLogger(EmcController.class);
 
@@ -33,8 +42,15 @@ public class EmcController {
 		ElmoTojava elmoTojava = new ElmoTojava();
 		String elmo = elmoTojava.frånGz64(emrexResponse.getElmo());
 		Elmo elmoparsed = elmoTojava.transformeraFrånXml(elmo);
+		// TODO: Verifiera signatur
 
 		logger.info("parsed  Elmo: " + elmoparsed.getLearner().getGivenNames());
+
+		byte[] elmo64 = Base64.getEncoder().encode(elmo.getBytes(StandardCharsets.UTF_8));
+
+		Response response = converterService.convertElmoToElm(new String(elmo64));
+
+		logger.warn("Converted response"+response);
 
 		// TODO: currently returning to "index"
 
